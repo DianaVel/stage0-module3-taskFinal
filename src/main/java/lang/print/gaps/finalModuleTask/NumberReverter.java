4fd920e2e48dfb9261f3baf1d8359574cd475134
diana veltauri <diana.veltauri1@gmail.com>
2022-07-21 16:45:16 +0400

    Initial commit

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result=0;
+        while(number>0){
+            result *=10;
+            result += number%10;
+            number = number/10;
+        }
+        System.out.println(result);
     }
 }
